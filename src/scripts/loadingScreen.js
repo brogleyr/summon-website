5511bffc49fce3efd1b5f342354ff291c10daf54
joan_the_narc <ryanbrogley@gmail.com>
2024-09-19 20:52:31 -0400

    spacing and comments

--- src/scripts/loadingScreen.js
+++ src/scripts/loadingScreen.js
@@ -1,22 +1,35 @@
-
+/**
+ * When called, expands loadingDiv downward in random jumps to simulate a slow
+ * page loading.
+ * @param {string} url - The url of the link to follow after the animation finishes
+ */
 function loadScreen(url) {
   console.log("loading!");
-  let id = null;
-  const elem = document.getElementById("loadingDiv");   
+
+  const loadingDiv = document.getElementById("loadingDiv");   
+
   let pos = 0;
+  let id = null;
   clearInterval(id);
-  id = setInterval(frame, 20);
-  function frame() {
+
+  // Calls scrollBg() every 20ms
+  id = setInterval(scrollBg, 20);
+
+  // Ex
+  function scrollBg() {
     if (pos >= window.innerHeight) {
       clearInterval(id);
-      window.open(url, "_self");
+      if (url) {
+        window.open(url, "_self");
+      }
     } else {
       pos += Math.floor(Math.pow(Math.random(), 5) * 50);
-      elem.style.height = pos + "px"; 
+      loadingDiv.style.height = pos + "px"; 
     }
   }
 }
 
+// Reloads the page when using history navigation. Added to reset loadingDiv.
 window.addEventListener( "pageshow", function ( event ) {
   var perfEntries = performance.getEntriesByType("navigation");
   if (perfEntries[0].type === "back_forward") {
